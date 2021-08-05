2c4fa3cf9ea5cbd2f0b9b92ffeff5897672ef7f5
Mazeli Victor <victor.vic.mazeli@gmail.com>
2021-08-05 07:09:23 +0100

    made changes

--- includes/dbconfig.php
+++ includes/dbconfig.php
@@ -1,9 +1,9 @@
 <?php
 date_default_timezone_set("Africa/Lagos");
-define('DB_SERVER','localhost');
-define('DB_USER','root');
-define('DB_PASS' ,'');
-define('DB_NAME', 'eedc');
+define('DB_SERVER','us-cdbr-east-04.cleardb.com');
+define('DB_USER','b345957a8898cc');
+define('DB_PASS' ,'7b490f5e');
+define('DB_NAME', 'heroku_940356f50cac20e');
 $conn = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
 if (mysqli_connect_errno())
